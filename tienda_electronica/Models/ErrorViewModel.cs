327389c9a0289bec86bca6d87917eea3f4c4f2cb
richard <TU_USUARIO@users.noreply.github.com>
2025-04-11 20:31:27 -0400

    Agregar archivos de proyecto.

---
+++ tienda_electronica/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace tienda_electronica.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
