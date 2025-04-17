633b69807e5ab565dd908d7f92656cbdbfe3ba77
richard <TU_USUARIO@users.noreply.github.com>
2025-04-16 22:12:43 -0400

    Se agrego un nuevo archivo html y css para los productos
    Esto nos ayudara a manejar la estrutura y estilos de la pagina de productos de la tienda

--- tienda_electronica/Program.cs
+++ tienda_electronica/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Productos}/{action=Index}/{id?}");
 
 app.Run();
