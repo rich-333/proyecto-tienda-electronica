0d53bd6b08ed431900cc83c315b7f7987b06d7e3
richard <TU_USUARIO@users.noreply.github.com>
2025-04-24 22:39:54 -0400

    Se agrego una nueva vista vaci y un archivo css
    En estos nuevos archivo crearemos una interfaz para agregar categorias.

--- tienda_electronica/Controllers/CategoriasController.cs
+++ tienda_electronica/Controllers/CategoriasController.cs
@@ -8,5 +8,10 @@ namespace tienda_electronica.Controllers
         {
             return View();
         }
+
+        public IActionResult Agregar()
+        {
+            return View();
+        }
     }
 }
