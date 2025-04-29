ea9ce727ba60a5abcbb80b4a2e767b8394d23ce0
richard <TU_USUARIO@users.noreply.github.com>
2025-04-28 20:59:38 -0400

    Se cambio el return del metodo Agregar en la clase productos controller.
    Esto permitira cargar nuestra vista agregar con un producto nuevo y evitar que sea null

--- tienda_electronica/Controllers/ProductosController.cs
+++ tienda_electronica/Controllers/ProductosController.cs
@@ -58,7 +58,7 @@ namespace tienda_electronica.Controllers
         {
             var categorias = categoriaData.ObtenerCategorias();
             ViewBag.Categorias = categorias;
-            return View();
+            return View(new Producto());
         }
 
         [HttpPost]
