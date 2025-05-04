00f064e7ba68c5b096b5febf47889681e1fa9ba9
richard <TU_USUARIO@users.noreply.github.com>
2025-05-03 21:25:06 -0400

    Se creo la clase Cuenta Data con los metodos autenticar usuario, autenticar cliente y registrar cliente. Ademas se creo la logica en Cuenta controller para llevar a paginas especificar segun el rol del usuario
    Los metodos creados nos ayudan a iniciar sesion verificando la base de datos.

--- tienda_electronica/Controllers/CuentaController.cs
+++ tienda_electronica/Controllers/CuentaController.cs
@@ -1,9 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using tienda_electronica.Data;
 
 namespace tienda_electronica.Controllers
 {
     public class CuentaController : Controller
     {
+        private readonly CuentaData cuentaData;
+
+        public CuentaController (IConfiguration config)
+        {
+            cuentaData = new CuentaData(config);
+        }
+
+        [HttpPost]
+        public IActionResult Login(string email, string contrasena)
+        {
+            var usuario = cuentaData.AutenticarUsuario(email, contrasena);
+            if (usuario != null) 
+            {
+                switch (usuario.rol) 
+                {
+                    case "Administrador":
+                    case "Gestor Productos":
+                        return RedirectToAction("Gestion", "Productos");
+                    case "Gestor Ventas":
+                        return RedirectToAction("Gestion", "Ventas");
+                }
+            }
+            
+            var cliente = cuentaData.AutenticarCliente(email, contrasena);
+            if (cliente != null) 
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            TempData["Error"] = "Credenciales Incorrectas.";
+            return RedirectToAction("Login");
+        }
+
+        [HttpGet]
         public IActionResult Login()
         {
             return View();
---
+++ tienda_electronica/Data/CuentaData.cs
@@ -0,0 +1,101 @@
+﻿using MySql.Data.MySqlClient;
+using tienda_electronica.Models.Usuarios;
+
+namespace tienda_electronica.Data
+{
+    public class CuentaData
+    {
+        private readonly Conexion _conexion;
+
+        public CuentaData (IConfiguration config)
+        {
+            _conexion = new Conexion(config);
+        }
+
+        public Usuario? AutenticarUsuario(string email, string contrasena)
+        {
+            using (var connection = _conexion.ObtenerConexion())
+            {
+                connection.Open();
+
+                var query = "SELECT * FROM usuarios WHERE email = @email AND contrasena = @contrasena";
+
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@contrasena", contrasena);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Usuario
+                            {
+                                idUsuario = Convert.ToInt32(reader["id_usuario"]),
+                                nombre = reader["nombre"].ToString(),
+                                apellido = reader["apellido"].ToString(),
+                                email = reader["email"].ToString(),
+                                contrasena = reader["contrasena"].ToString(),
+                                rutaFotoEmpleado = reader["ruta_foto_empleado"].ToString(),
+                                rol = reader["rol"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public Cliente ? AutenticarCliente (string email, string contrasena)
+        {
+            using (var connection = _conexion.ObtenerConexion())
+            {
+                connection.Open();
+
+                var query = "SELECT * FROM clientes WHERE email = @email AND contrasena = @contrasena";
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@email", email);
+                    cmd.Parameters.AddWithValue("@contrasena", contrasena);
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read()) 
+                        {
+                            return new Cliente
+                            {
+                                idCliente = Convert.ToInt32(reader["id_cliente"]),
+                                nombre = reader["nombre"].ToString(),
+                                apellido = reader["apellido"].ToString(),
+                                email = reader["email"].ToString(),
+                                contrasena = reader["contrasena"].ToString(),
+                                telefono = Convert.ToInt32(reader["telefono"]),
+                                direccion = reader["direccion"].ToString()
+                            };
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        public void RegistrarCliente(Cliente cliente)
+        {
+            using (var connection = _conexion.ObtenerConexion())
+            {
+                connection.Open();
+                var query = "INSERT INTO clientes (nombre, apellido, email, telefono, direccion, contrasena) VALUES (@nombre, @apellido, @email, @telefono, @direccion, @contrasena)";
+                using (var cmd = new MySqlCommand(query, connection))
+                {
+                    cmd.Parameters.AddWithValue("@nombre", cliente.nombre);
+                    cmd.Parameters.AddWithValue("@apellido", cliente.apellido);
+                    cmd.Parameters.AddWithValue("@email", cliente.email);
+                    cmd.Parameters.AddWithValue("@telefono", cliente.telefono);
+                    cmd.Parameters.AddWithValue("@direccion", cliente.direccion);
+                    cmd.Parameters.AddWithValue("@contrasena", cliente.contrasena);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
