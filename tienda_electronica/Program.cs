df8a1394e962e7190e13238a5a5033e10410c381
richard <TU_USUARIO@users.noreply.github.com>
2025-05-06 00:38:54 -0400

    Se agregaron cambios al padding del contenedor del nombre y precios del producto tanto en la vista inicio como en la vista index de productos.

--- tienda_electronica/Program.cs
+++ tienda_electronica/Program.cs
@@ -32,7 +32,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Clientes}/{action=ModificarInformacion}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
 
